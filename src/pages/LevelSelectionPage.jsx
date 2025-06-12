e4370b26318cbc4c40bf42c806cbe1f1c18bc341
adipermana16 <adiemokonkz16@gmail.com>
2025-06-12 01:28:03 -0700

    mengubah

--- src/pages/LevelSelectionPage.jsx
+++ src/pages/LevelSelectionPage.jsx
@@ -8,8 +8,8 @@ const LevelSelectionPage = () => {
     return (
         <div className="max-w-md mx-auto mt-5 p-4 bg-white rounded-xl shadow-md shadow-green-900 mb-5 hover:shadow-lg transition-all duration-300">
             <h1 className="text-center text-2xl font-bold justify-center items-center text-green-800">Pilih Level Kuis</h1>
-            <p className="text-gray-700 max-w-xl mx-auto text-base sm:text-lg mb-8">
-            Coba Kerjain Quiz, Yuk! Yuk, uji pemahaman kamu terhadap pengelolaan sampah!
+            <p className="text-gray-700 max-w-xl mx-auto text-base sm:text-lg mb-8 text-center justify-center items-center">
+            Coba Kerjain Quiz, Yuk! Uji pemahaman kamu terhadap pengelolaan sampah!
         </p>
             <ul className="grid grid-cols-2 gap-6">
                 {levels.map((level) => (
