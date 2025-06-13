7bb3033b79026590984064e0368499b14771d015
syauqic <89494804+syauqic@users.noreply.github.com>
2025-06-13 14:02:12 +0700

    revisi link API

--- src/components/InputGambar.jsx
+++ src/components/InputGambar.jsx
@@ -237,7 +237,7 @@ const InputGambar = () => {
       formData.append("file", file);
 
       const response = await axios.post(
-        "https://7f16-35-197-122-201.ngrok-free.app/predict",
+        "https://syauqic-recycle-api.hf.space/predict",
         formData,
         {
           headers: {
